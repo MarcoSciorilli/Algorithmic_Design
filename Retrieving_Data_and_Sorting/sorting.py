4a61fcad8fdc5a890e22f1b6243b9b7134cdc07b
MarcoSciorilli <sciorilli.marco@gmail.com>
2021-03-31 18:45:16 +0200

    generalizing dichotomic search

--- Retrieving_Data_and_Sorting/sorting.py
+++ Retrieving_Data_and_Sorting/sorting.py
@@ -1,14 +1,30 @@
-from typing import TypeVar, List, Union
+from typing import TypeVar, Any, Union, Optional
+from collections.abc import Callable
 
 T = TypeVar('T')
 
 
-def di_search(A: List[T], value: T) -> Union[None, int]:
+def min_order(a: T, b: T) -> bool:
+    return a <= b
+
+
+def di_search(A: Any, value: T, total_order = None) -> Union[None, int]:
     l = 0
     r = len(A) - 1
 
+    if total_order is None:
+        total_order = min_order
+
     while r >= 1:
         m = (l + r) // 2
+        if total_order(A[m], value):  # A[m].id <= value
+            if total_order(value, A[m]):  # A[m] >= value
+                return m
+
+            l = m + 1
+        else:
+            r = m - 1
+
         if A[m] == value:
             return m
 
